7a1b1b6b5e68e82fb0bc20732481d8f0279e62ff
anonmape <anonmape@greenwood.com>
2018-12-19 20:41:39 -0400

    Lecture04

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+(function () {
+// Declare Function
+'use strict';
 
+var x="hello";
+//we're able to execute things on this
+angular.module('myFirstApp', [])
+
+.controller('MyFirstController', function (){
+// Automatic binder
+}) //define the view model of our view (index.html)
+//[] is ann empty array
+
+})(); //no local variables bleed into the global scope
